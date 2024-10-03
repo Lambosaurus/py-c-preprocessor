62d87b414494e2e806f9307bceb31f64e5f2999c
NotYourFox <git@notyourfox.coffee>
2024-10-03 15:14:31 +0300

    Reverted accidental changes to test.h, again

--- tests/test_src/test.h
+++ tests/test_src/test.h
@@ -8,10 +8,10 @@
 #define MACRO_CONST         0x1
 
 #ifdef MACRO_CONST
-#define MACRO_A(a,b)        	(a + b)
-#define MACRO_B(a)          	(a + MACRO_CONST)
-#define MACRO_C(a,b)        	(MACRO_A(a, 1) + MACRO_B(b))
-#define MACRO_D(v)          	(v & (512 - 1))
+#define MACRO_A(a,b)        (a + b)
+#define MACRO_B(a)          (a + MACRO_CONST)
+#define MACRO_C(a,b)        (MACRO_A(a, 1) + MACRO_B(b))
+#define MACRO_D(v)          (v & (512 - 1))
 #else
 #error "This clause should not be reached"
 #endif
